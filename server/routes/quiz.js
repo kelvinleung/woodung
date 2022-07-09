62f5f70fc0f10dbd2b4c4d473aa4dc7acd5aa942
Kelvin <et.kelvin@gmail.com>
2022-07-09 23:32:55 +0800

    实现按 id 查询 quiz 接口

--- server/routes/quiz.js
+++ server/routes/quiz.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.post("/create", async (req, res) => {
   const { name, content } = req.body;
-  console.log(name, content);
   try {
     const quiz = await Quiz.create({ name, content });
     if (quiz) {
@@ -18,16 +17,59 @@ router.post("/create", async (req, res) => {
       throw new Error("Error creating quiz");
     }
   } catch (err) {
-    res.json({ code: 3000, message: "Error creating quiz", err });
+    res.json({ code: 3000, message: err.message || "Error creating quiz" });
+  }
+});
+
+router.post("/edit", async (req, res) => {
+  const { id } = req.query;
+  const { name, content } = req.body;
+  try {
+    const updateResult = await Quiz.update(
+      { name, content },
+      { where: { id } }
+    );
+    console.log(id, updateResult);
+    if (updateResult[0] === 1) {
+      return res.json({
+        code: 0,
+        message: "Quiz updated successfully",
+        id,
+      });
+    } else {
+      throw new Error("Quiz not found");
+    }
+  } catch (err) {
+    res.json({
+      code: 3000,
+      message: err.message || "Error updating quiz",
+    });
+  }
+});
+
+router.get("/", async (req, res) => {
+  const { id } = req.query;
+  try {
+    const quiz = await Quiz.findOne({ where: { id } });
+    if (quiz) {
+      return res.json({
+        code: 0,
+        data: quiz,
+      });
+    } else {
+      throw new Error("Quiz not found");
+    }
+  } catch (err) {
+    res.json({ code: 3000, message: err.message || "Error getting quiz" });
   }
 });
 
 router.get("/all", async (req, res) => {
   try {
-    const quizs = await Quiz.findAll();
-    return res.json({ code: 0, data: quizs });
+    const quizzes = await Quiz.findAll();
+    return res.json({ code: 0, data: quizzes });
   } catch (err) {
-    res.json({ code: 3000, message: "Error getting all quizs" });
+    res.json({ code: 3000, message: "Error getting all quizzes" });
   }
 });
 
